41341faee10f5da5282f677c10950eed678eecce
Benjamin Modayil <modayilme@gmail.com>
2017-09-29 14:07:01 -0400

    accessible nav tweaks plus weird jump on resize

--- src/javascripts/modules/mobile-nav.js
+++ src/javascripts/modules/mobile-nav.js
@@ -1,6 +1,18 @@
 const menuTrigger = document.querySelector('.global-navigation__icon-menu');
 const menuContainer = document.querySelector('.global-navigation__inner');
+const nav = document.querySelector('.global-navigation');
+const body = document.querySelector('body');
 
 menuTrigger.addEventListener('click', () => {
   menuContainer.classList.toggle('menu--active');
+  menuContainer.classList.add('menu--transition');
+  body.classList.toggle('menu--overflow');
+
+  if (menuContainer.classList.contains('menu--active')) {
+    nav.setAttribute('aria-expanded', 'true');
+  }
+  
+  else {
+    nav.setAttribute('aria-expanded', 'false');
+  }
 })
